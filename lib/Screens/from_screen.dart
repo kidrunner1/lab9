e88da841c363362ee3554f86789a8da3fdd280e3
Krit Daowaset <kidrunner1@gmail.com>
2023-03-03 22:26:20 +0700

    got bug Formscreen

--- lib/Screens/from_screen.dart
+++ lib/Screens/from_screen.dart
@@ -1,94 +1,115 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 import 'package:lab9/model/student.dart';
 
-class FromScreen extends StatefulWidget {
-  const FromScreen({super.key});
+class FormScreen extends StatefulWidget {
+  const FormScreen({super.key});
 
   @override
-  State<FromScreen> createState() => _FromScreenState();
+  State<FormScreen> createState() => _FormScreenState();
 }
 
-class _FromScreenState extends State<FromScreen> {
+class _FormScreenState extends State<FormScreen> {
   final formKey = GlobalKey<FormState>();
   Student myStudent = Student();
+  final Future<FirebaseApp> firebase = Firebase.initializeApp();
+  // ignore: prefer_final_fields
+  CollectionReference _studentCollection =
+      FirebaseFirestore.instance.collection("students");
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Input From'),
-        ),
-        body: Container(
-          padding: const EdgeInsets.all(20),
-          child: Form(
-            key: formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('Name', style: TextStyle(fontSize: 20)),
-                    TextFormField(
-                      validator: RequiredValidator(errorText: 'กรุณาป้อนชื่อ'),
-                      onSaved: (value) {
-                        myStudent.fname = value;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    const Text('Lastname', style: TextStyle(fontSize: 20)),
-                    TextFormField(
-                      validator:
-                          RequiredValidator(errorText: 'กรุณาป้อนนามสกุล'),
-                      onSaved: (value) {
-                        myStudent.lname = value;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    const Text('Email', style: TextStyle(fontSize: 20)),
-                    TextFormField(
-                      validator: RequiredValidator(errorText: 'กรุณาป้อนอีเมล'),
-                      onSaved: (value) {
-                        myStudent.email = value;
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    const Text('Temp', style: TextStyle(fontSize: 20)),
-                    TextFormField(
-                      validator:
-                          RequiredValidator(errorText: 'กรุณาป้อนอุณหภูมิ'),
-                      onSaved: (value) {
-                        myStudent.temp = value;
-                      },
-                      keyboardType: TextInputType.number,
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        child: const Text('Save'),
-                        style: ElevatedButton.styleFrom(
-                            textStyle: const TextStyle(fontSize: 20)),
-                        onPressed: () {
-                          if (formKey.currentState!.validate()) {
-                            formKey.currentState!.save();
-                            print("${myStudent.fname}");
-                            print("${myStudent.lname}");
-                            print("${myStudent.email}");
-                            print("${myStudent.temp}");
-                          }
-                        },
-                      ),
-                    )
-                  ]),
+    return FutureBuilder(
+        future: firebase,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Scaffold(
+              appBar: AppBar(title: Text('Eror')),
+              body: Center(child: Text('${snapshot.error}')),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.done) {}
+          return Scaffold(
+              body: Center(
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              child: Form(
+                key: formKey,
+                child: SingleChildScrollView(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('ชื่อ', style: TextStyle(fontSize: 20)),
+                        TextFormField(
+                          validator:
+                              RequiredValidator(errorText: 'กรุณาป้อนชื่อ'),
+                          onSaved: (value) {
+                            myStudent.fname = value;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        const Text('นามสกุล', style: TextStyle(fontSize: 20)),
+                        TextFormField(
+                          validator:
+                              RequiredValidator(errorText: 'กรุณาป้อนนามสกุล'),
+                          onSaved: (value) {
+                            myStudent.lname = value;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        const Text('อีเมล', style: TextStyle(fontSize: 20)),
+                        TextFormField(
+                          validator:
+                              RequiredValidator(errorText: 'กรุณาป้อนอีเมล'),
+                          onSaved: (value) {
+                            myStudent.email = value;
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        const Text('อุณหภูมิ', style: TextStyle(fontSize: 20)),
+                        TextFormField(
+                          validator:
+                              RequiredValidator(errorText: 'กรุณาป้อนอุณหภูมิ'),
+                          onSaved: (value) {
+                            myStudent.temp = value;
+                          },
+                          keyboardType: TextInputType.number,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                textStyle: const TextStyle(fontSize: 20)),
+                            onPressed: () async {
+                              if (formKey.currentState!.validate()) {
+                                formKey.currentState!.save();
+                                await _studentCollection.add({
+                                  'fname': myStudent.fname,
+                                  'lname': myStudent.lname,
+                                  'email': myStudent.email,
+                                  'temp': myStudent.temp,
+                                });
+                                formKey.currentState?.reset();
+                              }
+                            },
+                            child: const Text('บันทึก'),
+                          ),
+                        )
+                      ]),
+                ),
+              ),
             ),
-          ),
-        ));
+          ));
+        });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'Screens/dsiplay_screen.dart';
+import 'Screens/display.dart';
 import 'Screens/from_screen.dart';
 
 void main() {
@@ -10,21 +10,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -47,13 +37,13 @@ class _MyHomePageState extends State<MyHomePage> {
     return const DefaultTabController(
         length: 2,
         child: Scaffold(
-          body: TabBarView(children: [FromScreen(), DisplayScreen()]),
+          body: TabBarView(children: [FormScreen(), DisplayScreen()]),
           bottomNavigationBar: TabBar(tabs: [
             Tab(
-              text: 'get temp',
+              text: 'บันทึกอุณหภูมิ',
             ),
             Tab(
-              text: 'get name Student',
+              text: 'รายชื่อนักเรียน',
             )
           ]),
           backgroundColor: Colors.blue,
